53f8f4124336616fb805d06deb06363c6ede00d5
Technojerk36 <5809332+rogue-pulsar@users.noreply.github.com>
2021-12-06 03:28:30 -0500

    Day 6 completed

--- Day 6/6-2.py
+++ Day 6/6-2.py
@@ -1,36 +1,54 @@
 import csv
-import numpy
-import itertools
-
 #Import input as list
-with open('example_input.csv') as file:
+with open('input.csv') as file:
 	csvReader = csv.reader(file, delimiter = ',')
 	for row in csvReader:
 		fish_list = row
 #Convert list from string to int
 fish_list = list(map(int, fish_list))
-#Convert to numpy array
-fish_list = numpy.array(fish_list)
 
 #Time past
 day = 0
 
+#Fish states
+f0 = 0
+f1 = 0
+f2 = 0
+f3 = 0
+f4 = 0
+f5 = 0
+f6 = 0
+f7 = 0
+f8 = 0
+
+#Assign initial fish states
+for fish in range(len(fish_list)):
+	if fish_list[fish] == 1:
+		f1 += 1
+	if fish_list[fish] == 2:
+		f2 += 1
+	if fish_list[fish] == 3:
+		f3 += 1
+	if fish_list[fish] == 4:
+		f4 += 1
+	if fish_list[fish] == 5:
+		f5 += 1
+
 #Loop for each day
-while day < 18:
-	for fish in itertools.chain.from_iterable(fish_list):
-	#for fish in range(fish_list.size):
-		if fish_list[fish] == 0:
-			#Reset fish to 7 (not six since it gets processed today)
-			fish_list[fish] = 7
-			print(fish_list)
-			fish_list = numpy.append(fish_list, [8])
-		fish_list[fish] -= 1
+while day < 256:
+	temp = f0
+	f0 = f1
+	f1 = f2
+	f2 = f3
+	f3 = f4
+	f4 = f5
+	f5 = f6
+	f6 = f7 + temp
+	f7 = f8
+	f8 = temp
+
 	day += 1
 	print('It is day:', day)
-	print('There are:', len(fish_list), 'fish.')
+	print('There are:', f0 + f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8, 'fish.')
 
-
-#Result = 
-#print(len(fish_list))
-
-#Started at 1:42am
+#Result = 1617359101538
